06c7b38239aaa9db8008e36cc3d5befe5230b55f
jesus doza <jesus.doza@gmail.com>
2023-05-28 13:48:01 -0500

    76 updating deployments

--- postService/index.js
+++ postService/index.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser");
 const { randomBytes } = require("crypto");
 const cors = require("cors");
 const axios = require("axios");
+const { log } = require("console");
 
 const app = express();
 app.use(bodyParser.json());
@@ -11,35 +12,36 @@ app.use(cors());
 const posts = {};
 
 app.get("/posts", (req, res) => {
-  res.send(posts);
+    res.send(posts);
 });
 
 app.post("/posts", async (req, res) => {
-  const id = randomBytes(4).toString("hex");
-  const { title } = req.body;
+    const id = randomBytes(4).toString("hex");
+    const { title } = req.body;
 
-  posts[id] = {
-    id,
-    title,
-  };
+    posts[id] = {
+        id,
+        title,
+    };
 
-  await axios.post("http://localhost:4005/events", {
-    type: "PostCreated",
-    data: {
-      id,
-      title,
-    },
-  });
+    await axios.post("http://localhost:4005/events", {
+        type: "PostCreated",
+        data: {
+            id,
+            title,
+        },
+    });
 
-  res.status(201).send(posts[id]);
+    res.status(201).send(posts[id]);
 });
 
 app.post("/events", (req, res) => {
-  console.log("Received Event", req.body.type);
+    console.log("Received Event", req.body.type);
 
-  res.send({});
+    res.send({});
 });
 
 app.listen(4000, () => {
-  console.log("Listening on 4000");
+    console.log("v55");
+    console.log("Listening on 4000");
 });
